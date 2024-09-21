0f7e7b459bd5938dbb91e11e0e9038c649675aa3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 01:24:45 -0400

    Create getPreSalesTaxPrice() and getSalesTaxInput(); add totalLabor and totalExpenses to the class constructor; totalPricePreSalesTax now logs successfully!

--- main.js
+++ main.js
@@ -16,24 +16,40 @@ function getShippingCostInput() {
     const shippingCostInput = document.getElementById('shipping-cost').value;
     return Number(shippingCostInput);
 }
+function getSalesTaxInput() {
+    const salesTaxInput = document.getElementById('sales-tax').value;
+    return Number(salesTaxInput);
+}
+
 class GetPrice {
-    constructor(laborHoursInput, hourlyRateInput, materialsCostInput, shippingCostInput) {
+    constructor(laborHoursInput, hourlyRateInput, materialsCostInput, shippingCostInput, totalLaborRate, totalExpenses) {
         this.laborHoursInput = getLaborHoursInput();
         this.hourlyRateInput = getHourlyRateInput();
         this.materialsCostInput = getMaterialsCostInput();
         this.shippingCostInput = getShippingCostInput();
+        this.totalLaborRate = this.getTotalLaborRate();
+        this.totalExpenses = this.getTotalExpenses();
     }
     getTotalLaborRate() {
         const totalLaborRate = this.laborHoursInput * this.hourlyRateInput;
         labor.innerHTML += `<p>Total labor rate: ${this.laborHoursInput} hours at $${this.hourlyRateInput}/hour = $${totalLaborRate}.</p>`
-        return totalLaborRate;
+        return Number(totalLaborRate);
     }
     getTotalExpenses() {
         //const materialsCost = this.materialsCostInput;
         //const shippingCost = this.shippingCostInput;
         const totalExpenses = this.materialsCostInput + this.shippingCostInput;
         labor.innerHTML += `<p>Total expenses: $${this.materialsCostInput} in materials + $${this.shippingCostInput} in shipping = $${totalExpenses}.</p>`
-        return totalExpenses;
+        return Number(totalExpenses);
+    }
+    getPreSalesTaxPrice() {
+        const totalPricePreSalesTax = this.totalLaborRate + this.totalExpenses;
+        console.log(totalPricePreSalesTax)
+        return Number(totalPricePreSalesTax);
+    }
+    applySalesTax() {
+        const totalPriceWithSalesTaxAddedOn = ((totalPricePreSalesTax) * salesTaxInput) + totalPricePreSalesTax;
+        return Number(totalPriceWithSalesTaxAddedOn);
     }
 }
 
@@ -42,4 +58,5 @@ document.getElementById('submit-btn').addEventListener("click", function (event)
     const newRate = new GetPrice();
     newRate.getTotalLaborRate();
     newRate.getTotalExpenses();
+    newRate.getPreSalesTaxPrice();
 });
