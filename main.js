d183d7442330afb3474433bfc12bc0e1681e20fe
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 00:55:45 -0400

    Add getShippingPrice() and add shippingCostInput to the class constructor

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ function getHourlyRateInput() {
     const hourlyRateInput = document.getElementById('labor-hourly-rate').value;
     return hourlyRateInput;
 }
-function getMaterialsCost() {
+function getMaterialsCostInput() {
     const materialsCostInput = document.getElementById('materials-cost').value;
     return materialsCostInput;
 }
+function getShippingCostInput() {
+    const shippingCostInput = document.getElementById('shipping-cost').value;
+    return shippingCostInput;
+}
 class GetPrice {
-    constructor(laborHoursInput, hourlyRateInput, materialsCostInput) {
+    constructor(laborHoursInput, hourlyRateInput, materialsCostInput, shippingCostInput) {
         this.laborHoursInput = getLaborHoursInput();
         this.hourlyRateInput = getHourlyRateInput();
-        this.materialsCostInput = getMaterialsCost();
+        this.materialsCostInput = getMaterialsCostInput();
+        this.shippingCostInput = getShippingCostInput();
     }
     getTotalLaborRate() {
         const totalLaborRate = this.laborHoursInput * this.hourlyRateInput;
