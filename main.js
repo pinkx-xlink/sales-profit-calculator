b61fbdbd1ed4b767e23d10da648da9862c15d535
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 01:08:05 -0400

    Troubleshoot getTotalExpenses - it's concatenating the inputs instead of actually adding them together.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function getHourlyRateInput() {
 }
 function getMaterialsCostInput() {
     const materialsCostInput = document.getElementById('materials-cost').value;
+    // const num1 = materialsCostInput.Number();
     return materialsCostInput;
 }
 function getShippingCostInput() {
@@ -27,10 +28,18 @@ class GetPrice {
         labor.innerHTML += `<p>Total labor rate: ${this.laborHoursInput} hours at $${this.hourlyRateInput}/hour = $${totalLaborRate}.</p>`
         return totalLaborRate;
     }
+    getTotalExpenses() {
+        //const materialsCost = this.materialsCostInput;
+        //const shippingCost = this.shippingCostInput;
+        const totalExpenses = this.materialsCost + this.shippingCost;
+        labor.innerHTML += `<p>Total expenses: $${this.materialsCostInput} in materials + $${this.shippingCostInput} in shipping = $${totalExpenses}.</p>`
+        return totalExpenses;
+    }
 }
 
 document.getElementById('submit-btn').addEventListener("click", function (event) {
     event.preventDefault();
     const newRate = new GetPrice();
     newRate.getTotalLaborRate();
+    newRate.getTotalExpenses();
 });
