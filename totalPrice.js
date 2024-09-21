0bf3e0624c727bc207a80834ab66cb69b46e8353
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-20 22:05:48 -0400

    Add submit button and a console log statement on click

--- totalPrice.js
+++ totalPrice.js
@@ -2,10 +2,11 @@ const laborRate = require('./laborRate');
 const getExpenses = require('./getExpenses');
 const salesTax = require('./salesTax');
 
-const laborRateInput = document.getElementById('labor-hours');
+const laborRateInputFeild = document.getElementById('labor-hours');
+const laborRateInput = laborRateInputFeild.value;
 
 const submitBtn = document.getElementById('submit');
-submitBtn.addEventListener('click', function() {
+submitBtn.addEventListener('click', function () {
     console.log(laborRateInput);
 })
 
