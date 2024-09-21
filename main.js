97c279761bbb9d39679928e46b9d8e0ac9c4a0d7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 18:21:18 -0400

    Delete all commented out method calls from the submitBtn event listener

--- main.js
+++ main.js
@@ -62,11 +62,6 @@ class GetPrice {
 
 document.getElementById('submit-btn').addEventListener("click", function (event) {
   event.preventDefault();
-  const newRate = new GetPrice();
-  // newRate.getTotalLaborRate();
-  // newRate.getTotalExpenses();
-  // newRate.getPreSalesTaxPrice();
-  // newRate.applySalesTax();
   totalPrice.innerHTML += `<p>To earn a profit of 
   $${newRate.totalLaborRate} for this product, you will 
   need to charge $${newRate.totalPriceWithSalesTaxAddedOn} 
