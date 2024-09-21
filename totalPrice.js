209b7a68610dbdeb6e0b72f7706aa966f4788c9d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-20 21:59:07 -0400

    Add stylesheet

--- totalPrice.js
+++ totalPrice.js
@@ -2,5 +2,11 @@ const laborRate = require('./laborRate');
 const getExpenses = require('./getExpenses');
 const salesTax = require('./salesTax');
 
+const laborRateInput = document.getElementById('labor-hours');
+
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', function() {
+    console.log(laborRateInput);
+})
 
 // function getTotalExpenses() {}
