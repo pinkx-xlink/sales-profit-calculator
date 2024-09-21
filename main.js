75f5eeafa4497e79c682ab6ef1057c9bb2749fad
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 00:23:17 -0400

    Create GetLaborRate class

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-document.getElementById('submit-btn').addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log('blah');
-    getLaborHoursInput();
-})
-
 console.log('is this thing on')
 
 function getLaborHoursInput() {
-    const laborRateInput = document.getElementById('labor-hours').value;
-    console.log(laborRateInput);
-    return laborRateInput;
+    const laborHoursInput = document.getElementById('labor-hours').value;
+    return laborHoursInput;
 }
+function getHourlyRateInput() {
+    const hourlyRateInput = document.getElementById('labor-hourly-rate').value;
+    return hourlyRateInput;
+}
+class GetLaborRate {
+    constructor(laborHoursInput, hourlyRateInput) {
+        this.laborHoursInput = getLaborHoursInput();
+        this.hourlyRateInput = getHourlyRateInput();
+        this.totalLaborRate = laborHoursInput * hourlyRateInput;
+    }
+}
+
+document.getElementById('submit-btn').addEventListener("click", function (event) {
+    event.preventDefault();
+    const newRate = new GetLaborRate();
+    console.log(newRate);
+});
+
+// console.log(getLaborRate(laborHoursInput, totalLaborRate));
