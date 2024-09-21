3a855f94b1416753476bdd468ee9400a686eeda2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-20 23:45:57 -0400

    Add defer to script; refactor button and add event listener

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const submitBtn = document.getElementById('submit-btn');
-submitBtn.addEventListener('submit', function (event) {
+document.getElementById('submit-btn').addEventListener("click", function (event) {
+    event.preventDefault();
     console.log('blah');
+    getLaborHoursInput();
 })
 
 console.log('is this thing on')
 
 function getLaborHoursInput() {
     const laborRateInput = document.getElementById('labor-hours').value;
+    console.log(laborRateInput);
     return laborRateInput;
 }
