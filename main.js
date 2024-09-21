b02137d2ce3143045b2333ce70840d278d112ad2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-21 00:33:59 -0400

    Create a new labor rate based on input when the button is clicked

--- main.js
+++ main.js
@@ -12,14 +12,18 @@ class GetLaborRate {
     constructor(laborHoursInput, hourlyRateInput) {
         this.laborHoursInput = getLaborHoursInput();
         this.hourlyRateInput = getHourlyRateInput();
-        this.totalLaborRate = laborHoursInput * hourlyRateInput;
+        // this.totalLaborRate = laborHoursInput * hourlyRateInput;
+    }
+    getTotalLaborRate() {
+        const totalLaborRate = this.laborHoursInput * this.hourlyRateInput;
+        return totalLaborRate;
     }
 }
 
 document.getElementById('submit-btn').addEventListener("click", function (event) {
     event.preventDefault();
     const newRate = new GetLaborRate();
-    console.log(newRate);
+    console.log(newRate.getTotalLaborRate());
 });
 
 // console.log(getLaborRate(laborHoursInput, totalLaborRate));
