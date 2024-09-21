fab5d462c02e4f74b0cdc08d9c0bcced8b5545f1
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-20 21:12:08 -0400

    Create totalPricePlusTax variable

--- salesTax.js
+++ salesTax.js
@@ -1,5 +1,6 @@
 const laborRate = require('./laborRate');
 const getExpenses = require('./getExpenses');
-function getSalesTax(getExpenses, totalLaborRate, salesTax) {
-
+function getSalesTax(getExpenses, totalLaborRate) {
+  const totalPricePlusTax = ((getExpenses + totalLaborRate) * 0.07) + (getExpenses + totalLaborRate);
+  return totalPricePlusTax;
 }
