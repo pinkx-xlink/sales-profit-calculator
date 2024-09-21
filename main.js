3d8c9d2a01846cea011a4c4988703ae94cf9e91c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-09-20 23:27:17 -0400

    Move code from totalPrice.js => main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const laborRateInputField = document.getElementById('labor-hours').value;
+const laborRateInput = laborRateInputField;
+
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', function () {
+    console.log(laborRateInput);
+})
+
+function getTotalPrice() {
+    console.log('hi');
+    return 'hi';
+}
--- totalPrice.js
+++ totalPrice.js
@@ -2,15 +2,3 @@ const laborRate = require('./laborRate');
 const getExpenses = require('./getExpenses');
 const salesTax = require('./salesTax');
 
-const laborRateInputFeild = document.getElementById('labor-hours');
-const laborRateInput = laborRateInputFeild.value;
-
-const submitBtn = document.getElementById('submit');
-submitBtn.addEventListener('click', function () {
-    console.log(laborRateInput);
-})
-
-function getTotalPrice() {
-    console.log('hi');
-    return 'hi';
-}
